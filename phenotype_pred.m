12611bc83fc8da5a21d7c825b4845a59f7f58e55
Julie Shade <jshade1@jhu.edu>
2018-04-05 16:52:46 -0400

    finished param opt and t_80

--- phenotype_pred.m
+++ phenotype_pred.m
@@ -32,29 +32,47 @@ end
 R = abs(R);
 [R_sorted, ind] = sort(R, 'descend');
 
-numgenes = [1:10:200, 300:100:1000]
-a = 1
+numgenes = [1,10:10:200, 300:100:1000];
+a = 1;
 for i = numgenes
     D_hat = ind(1:i);
     X = [ones(size(Xtrain,2),1), Xtrain(D_hat,:)'];
     w = pinv(X'*X)*X'*Ytrain;
     Xtest_cond = [ones(size(Xtest,2),1),Xtest(D_hat,:)'];
-    p_hat_test = 1./(1+exp(w'*Xtest_cond'));
-    for t = 0:.01:1
+    p_hat_test = 1./(1+exp(w'*Xtest_cond')); % calculate discriminant
+    for t = 0:.001:1
         Yhat_test = zeros(1,size(Xtest,2));
         Yhat_test(find(p_hat_test >= t))=1;
         CP = classperf(Ytest,Yhat_test);
-        sens(int16(t*100)+1,a) = CP.sensitivity;
-        spec(int16(t*100)+1,a) = CP.specificity;
-        acc(int16(t*100)+1,a) = CP.CorrectRate;
+        sens(int16(t*1000)+1,a) = CP.sensitivity;
+        spec(int16(t*1000)+1,a) = CP.specificity;
+        acc(int16(t*1000)+1,a) = CP.CorrectRate;
     end
     a = a+1;
 end
 
-[Mr,Ic] = max(acc)
-[Mall,Ir] = max(Mr)
+[Mr,Ic] = max(acc);
+[Mall,Ir] = max(Mr);
 
-rLabels(ind(1))
+t = 0:.001:1;
+numgenesopt = numgenes(Ir); % number of genes that gives the maximum accuracy
+sensopt = sens(:, Ir);
+specopt = spec(:, Ir);
+
+% plot ROC curve
+figure(1)
+plot(1-specopt, sensopt)
+hold on;
+xlabel('1-spec(t)')
+ylabel('sens(t)')
+title('ROC Curve for NodalStatus Classifier')
+
+% find values of t for which sensivitity(t) >= .8
+t_80_index = min(find(sensopt >= .8));
+t_80 = t(t_80_index)
+% find specificity at 80% sensitivity
+spec_80 = specopt(t_80_index)
+
 
 
 
